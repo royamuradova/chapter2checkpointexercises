31c66786f5e39b49bad0f6d3bb04298bdbf0e9a0
royamuradova <royamuradova1998@gmail.com>
2024-09-20 00:17:54 +0000

    i runned and verified checkpointexercise2.14

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,16 @@
+//ROYA
+//09192024
+//CHAPTER 2 CHECKPOINT EXERCISE
+//CHECKPOINTEXERCISE2.14
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main()
+{
+char first, middle, last;
+first = 'R';
+middle = 'R';
+last = 'M';
+cout << first << " " << middle << " " << last << endl;
+return 0;
+}
